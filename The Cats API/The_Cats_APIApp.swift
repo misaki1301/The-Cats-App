2b66042f6d6680c6d652970f257852559524bbbe
misaki1301 <paul.frankpc@gmail.com>
2023-02-26 16:33:25 -0500

    Initial Commit

---
+++ The Cats API/The_Cats_APIApp.swift
@@ -0,0 +1,17 @@
+//
+//  The_Cats_APIApp.swift
+//  The Cats API
+//
+//  Created by Paul Frank Pacheco Carpio on 26/02/23.
+//
+
+import SwiftUI
+
+@main
+struct The_Cats_APIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
